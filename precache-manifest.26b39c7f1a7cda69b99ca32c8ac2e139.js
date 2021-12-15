024af4c9b16182ecac8ac8aae068113c3bba238c
olegweremyew <olegweremey1994@mail.ru>
2021-12-15 20:39:49 +0300

    Updates

---
+++ precache-manifest.26b39c7f1a7cda69b99ca32c8ac2e139.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4291d3e84cff951413418a1c06dab7f",
+    "url": "/MyToDoList/index.html"
+  },
+  {
+    "revision": "5e28bcc7385732bf9853",
+    "url": "/MyToDoList/static/css/main.d780d2b8.chunk.css"
+  },
+  {
+    "revision": "ab5bc26f329b9fd7e039",
+    "url": "/MyToDoList/static/js/2.272c6e2b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/MyToDoList/static/js/2.272c6e2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e28bcc7385732bf9853",
+    "url": "/MyToDoList/static/js/main.9ce35833.chunk.js"
+  },
+  {
+    "revision": "3a34703f4c835407aa06",
+    "url": "/MyToDoList/static/js/runtime-main.7df911c2.js"
+  }
+]);
